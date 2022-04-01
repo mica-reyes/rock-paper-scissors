5ac97e2f21ae843071c8c7b6752049e3bf304762
mica-reyes <reyessmicaela@gmail.com>
2022-04-01 19:40:08 -0300

    add ui

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rockBtn = document.querySelector('.fa-hand-back-fist');
+const paperBtn = document.querySelector('.fa-hand');
+const scissorsBtn = document.querySelector('.fa-hand-scissors');
+const playerHand = document.querySelector('#playerHand');
+const computerHand = document.querySelector('#computerHand');
+const playerScore = document.querySelector('#playerScore')
+const computerScore = document.querySelector('#computerScore')
+
 const computerPlay = () => {
     let computer = Math.ceil(Math.random() * 3)
     switch (computer) {
@@ -10,21 +18,38 @@ const computerPlay = () => {
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection == computerSelection) return 'tie';
     if (playerSelection == 'rock') return computerSelection == 'scissors' ? 'player ' : 'computer';
-    if (playerSelection =='paper') return computerSelection == 'rock'? 'player': 'computer';
-    if (playerSelection == 'scissors') return computerSelection== 'paper'? 'player':'computer';
+    if (playerSelection == 'paper') return computerSelection == 'rock' ? 'player' : 'computer';
+    if (playerSelection == 'scissors') return computerSelection == 'paper' ? 'player' : 'computer';
 }
 
-const game = () => {
-    let cont = 5;
-    let playerContador = 0;
-    let computerContador = 0;
-    while (cont >= 1) {
-        const playerSelection = prompt('rock paper scissors');
-        const computerSelection = computerPlay();
-        const winner = playRound(playerSelection, computerSelection);
-        if (winner !== 'tie') winner === 'computer' ? computerContador++ : playerContador++;
-        cont--;
-    }
+let playerContador = 0;
+let computerContador = 0;
+
+const addClassList = (selection) => {
+    if (selection === 'rock') return "fa-hand-back-fist"
+    if (selection === 'paper') return "fa-hand"
+    if (selection === 'scissors') return "fa-hand-scissors"
 }
 
-game()
+const addClass = (playerSelection, computerSelection) => {
+    let playerClassList = addClassList(playerSelection)
+    let computerClassList = addClassList(computerSelection)
+    playerHand.classList.add(playerClassList)
+    computerHand.classList.add(computerClassList)
+}
+
+
+const play = (e) => {
+    if (playerHand.classList[1]) playerHand.classList.toggle(playerHand.classList[1])
+    if (computerHand.classList[1]) computerHand.classList.toggle(computerHand.classList[1])
+    const playerSelection = e.target.dataset.key;
+    const computerSelection = computerPlay();
+    addClass(playerSelection, computerSelection)
+    const winner = playRound(playerSelection, computerSelection);
+    if (winner !== 'tie') winner === 'computer' ? computerContador++ : playerContador++;
+    playerScore.innerText = 'player: ' + playerContador
+    computerScore.innerText= ' computer :' + computerContador
+}
+rockBtn.addEventListener('click', play)
+paperBtn.addEventListener('click', play)
+scissorsBtn.addEventListener('click', play)
